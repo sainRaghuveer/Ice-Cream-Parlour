20427afeda70af7e80462105a0afe6eb3eff78d1
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-10 23:43:45 +0530

    Redux folder structure

--- frontend/src/Redux/Store.js
+++ frontend/src/Redux/Store.js
@@ -1,4 +1,4 @@
-import {legacy_createStore, combineReducers, applyMiddleware} from "redux";
+import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 
