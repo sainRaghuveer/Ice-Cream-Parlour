fdc410bfde1c527910368b98439ab3a0c459711b
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-10 23:27:36 +0530

    frontend folder structure

--- frontend/src/routes/AllRoutes.jsx
+++ frontend/src/routes/AllRoutes.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from "react-router-dom"
+import IceCream from '../pages/customers/IceCream';
+import Cart from '../pages/customers/Cart';
+import AddIcecream from '../pages/Admin/AddIcecream';
+import Inventory from '../pages/Admin/Inventory';
+import Login from '../pages/Admin/Login';
 
 const AllRoutes = () => {
   return (
-    <div>AllRoutes</div>
+    <div>
+        <Routes>
+            {/*---------------- Customer routes --------------- */}
+            <Route path='/' element={<IceCream/>}></Route>
+            <Route path='/cart' element={<Cart/>}></Route>
+
+            {/*---------------- Admin routes --------------- */}
+            <Route path='/addicecream' element={<AddIcecream/>}></Route>
+            <Route path='/inventory' element={<Inventory/>}></Route>
+            <Route path='/login' element={<Login/>}></Route>
+        </Routes>
+    </div>
   )
 }
 
