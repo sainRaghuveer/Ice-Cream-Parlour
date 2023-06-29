7fda8dde2639303a34ea9e6cab7b8abf894b9673
sainRaghuveer <raghuveersain987@gmail.com>
2023-06-29 20:58:38 +0530

    updates

--- frontend/src/components/Skeleton.jsx
+++ frontend/src/components/Skeleton.jsx
@@ -1,7 +1,6 @@
 import { Skeleton, SkeletonCircle, SkeletonText, Stack, Box } from '@chakra-ui/react';
 import React, { useState } from "react"
 export const SkeletonE = () => {
-  const [isLoaded, setIsLoaded] = useState(false)
   return (
     <Stack padding={4} spacing={1} marginLeft={"10px"} marginRight={"10px"} margin={"auto"}>
       <Skeleton
